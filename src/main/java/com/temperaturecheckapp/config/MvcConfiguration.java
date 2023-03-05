fe8b80bf4ac633c9cf57b64cca531238eeb3b7f0
NikhilSarode <nikhilsarode29@yahoo.in>
2023-03-05 15:43:57 +0530

    build changes

--- src/main/java/com/temperaturecheckapp/config/MvcConfiguration.java
+++ src/main/java/com/temperaturecheckapp/config/MvcConfiguration.java
@@ -17,7 +17,7 @@ public class MvcConfiguration implements WebMvcConfigurer{
 	@Bean
 	public ViewResolver getViewResolver(){
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
+		//resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
